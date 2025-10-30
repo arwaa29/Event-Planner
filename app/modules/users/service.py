a85689e60ddb601bb5dbfab86c2c0ac89e6b1c73
nourtarek885 <nourtarek885@gmail.com>
2025-10-30 22:16:49 +0300

    Update service.py

---
+++ app/modules/users/service.py
@@ -0,0 +1,51 @@
+import bcrypt
+from app.modules.users.models import userTable
+idCounter = 0
+
+class userManagement:
+
+    # All classes have a built-in method called __init__(), which is always executed when the class is being initiated.
+    # Without self, Python would not know which object's properties you want to access
+
+    # signup
+    def signUp(self, name, email, username, password):
+        if userTable.find_one({"email": email}):
+            print("User Already Exists")
+            return
+        global idCounter
+        user = {"_id": idCounter, "name": name, "email": email, "username": username, "password": bcrypt.hashpw(password.encode("utf8"), bcrypt.gensalt())}
+        userTable.insert_one(user)
+        idCounter += 1
+        print("Sign Up Successfully!")
+
+    # login email
+    def loginByEmail(self, email, password):
+        if userTable.find_one({"email": email}):
+            print("Logged In")
+        else:
+            print("Login Failed")
+
+    # login username
+    def loginByUsername(self, username,password):
+        if userTable.find_one({"username": username}):
+            print("Logged In")
+        else:
+            print("Login Failed")
+
+# test
+# user1 = {"name": "Nour","email":"Nourtarek885@gmail.com", "username": "Nourtarek885","password":""}
+# userTable.insert_one(user1)
+# userTable.insert_one({"name":"Arwa", "email":"Arwa@gmail.com"})
+# eventTable.insert_one({"title":"", "date":"", "time": "", "location": "", "description": ""})
+# print(dataBase.list_collection_names())
+
+userManagement = userManagement()
+userManagement.signUp("Nour", "nour@gmail.com", "nourtarek885", "NourTarek11")
+
+userStr = input("Enter your username or email: ")
+password = input("Enter your password: ")
+
+if "@" in userStr:
+    userManagement.loginByEmail(userStr, password)
+else:
+    userManagement.loginByUsername(userStr, password)
