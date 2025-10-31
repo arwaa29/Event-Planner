242fce68da475b218644fde66b73d5d288a0cf11
arwaa29 <140168581+arwaa29@users.noreply.github.com>
2025-10-31 22:53:53 +0200

    DB connection

--- app/database.py
+++ app/database.py
@@ -10,3 +10,20 @@ dataBase = client['EventPlanner']
 
 # Collections
 user_collection = dataBase["users"]
+
+
+# import motor.motor_asyncio
+# import asyncio
+#
+# MONGO_URI = "mongodb+srv://nourtarek885_db_user:MyStrongPassword123@cluster0.wknjzi0.mongodb.net/myAppDB?retryWrites=true&w=majority&appName=Cluster0"
+#
+# async def test_connection():
+#     client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI, tls=True, tlsAllowInvalidCertificates=True)
+#     db = client["EventPlanner"]
+#     try:
+#         await db.command("ping")
+#         print("✅ Connected successfully to MongoDB Atlas!")
+#     except Exception as e:
+#         print("❌ Connection failed:", e)
+#
+# asyncio.run(test_connection())
