706de07f82618bb6aef92ddf907c81518c914d8c
arwaa29 <140168581+arwaa29@users.noreply.github.com>
2025-10-31 18:29:16 +0200

    validate data

--- app/modules/users/schemas.py
+++ app/modules/users/schemas.py
@@ -1,15 +1,35 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
 
 class Signup(BaseModel):
         first_name: str
         last_name: str
         email: EmailStr
         username: str
-        password: str
-        confirm_password: str
-        role: str
+        password: str = Field(..., min_length=6)
+        role: Optional[str] = "attendee"
 
 class Login(BaseModel):
     email: EmailStr
     username: str
     password: str
+
+class userResponse(BaseModel):
+    id: str
+    first_name: str
+    last_name: str
+    email: EmailStr
+    username: str
+    role: str
+
+class RegisterResponse(BaseModel):
+    message: str
+    token: str
+
+class LoginResponse(BaseModel):
+    message: str
+    token: str
+
+model_config = {
+    "from_attributes": True
+}
