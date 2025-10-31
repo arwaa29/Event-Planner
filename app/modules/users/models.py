09ec5e7f19fc2b1023d7eeeb6ca4e3ab359e3098
arwaa29 <140168581+arwaa29@users.noreply.github.com>
2025-10-31 18:32:04 +0200

    helper

--- app/modules/users/models.py
+++ app/modules/users/models.py
@@ -1,3 +1,14 @@
-from app.database import dataBase
+#when sed data from back to front ,
+#this will help in converting mongoDB into JSON friendly format like response we write in schema.py
 
-userTable = dataBase["user"]
+from bson import ObjectId
+
+def user_helper(user) -> dict:
+    return {
+        "id": str(user["_id"]),
+        "first_name": user["first_name"],
+        "last_name": user["last_name"],
+        "email": user["email"],
+        "username": user["username"],
+        "role": user["role"],
+    }
