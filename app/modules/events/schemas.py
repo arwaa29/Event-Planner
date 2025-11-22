a57a6d6abec47d2e5068ed00af795fe0900e95e1
nourtarek885 <nourtarek885@gmail.com>
2025-11-22 19:51:13 +0200

    Update schemas.py

--- app/modules/events/schemas.py
+++ app/modules/events/schemas.py
@@ -1,1 +1,21 @@
 from pydantic import BaseModel
+
+class CreateEvent(BaseModel):
+        title: str
+        date: str
+        time: str
+        location: str
+        description: str
+        organizer: str
+        attendees: list[dict]
+
+class InviteUser(BaseModel):
+    event_id: str
+    user: str
+
+class EventResponse(BaseModel):
+    message: str
+    
+model_config = {
+    "from_attributes": True
+}
