9b6649a8e680669e21edd4ac1f3222b4e220ef02
nourtarek885 <nourtarek885@gmail.com>
2025-11-22 13:20:34 +0200

    Update models.py

---
+++ app/modules/events/models.py
@@ -0,0 +1,9 @@
+def event_helper(event) -> dict:
+    return {
+        "id": str(event["_id"]),
+        "title": event["title"],
+        "date": event["date"],
+        "time": event["time"],
+        "location": event["location"],
+        "description": event["description"],
+    }
