8a19d44d13dd2b71fb406a434aea3476cd507361
nourtarek885 <nourtarek885@gmail.com>
2025-11-23 19:51:12 +0200

    Update service.py

---
+++ app/modules/search/service.py
@@ -0,0 +1,24 @@
+from app.database import dataBase
+from app.auth.jwt_handler import createAccessToken
+
+async def searchByName(title: str) -> dict:
+    return await dataBase.Events.find({"title":{"$regex": title}})
+
+async def searchByTaskDescrip(task: str) -> dict:
+    return await dataBase.Events.find({"description": {"$regex": task}})
+
+async def dataFiltering(date: str) -> dict:
+    return await dataBase.Events.find({"date":{"$regex": date}})
+
+async def userFiltering(role: str) -> dict:
+    return await dataBase.Events.find({"attendees.user":{"$regex": role}})
+
+async def combination(title = None, description = None, date = None, role = None) -> dict:
+    if title:
+        searchByName(title)
+    if description:
+        searchByTaskDescrip(description)
+    if date:
+        dataFiltering(date)
+    if role:
+        userFiltering(role)
