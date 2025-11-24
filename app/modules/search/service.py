83ca083a97ecd0efb30b80a7f32b1c5a9c6cf813
nourtarek885 <nourtarek885@gmail.com>
2025-11-24 15:55:01 +0200

    Update service.py

--- app/modules/search/service.py
+++ app/modules/search/service.py
@@ -1,24 +1,27 @@
 from app.database import dataBase
 from app.auth.jwt_handler import createAccessToken
 
-async def searchByName(title: str) -> dict:
-    return await dataBase.Events.find({"title":{"$regex": title}})
+async def searchByName(title: str):
+    return await dataBase.Events.find({"title":{"$regex": title, "$options": "i"}}).to_list(length=None)
 
-async def searchByTaskDescrip(task: str) -> dict:
-    return await dataBase.Events.find({"description": {"$regex": task}})
+async def searchByTaskDescrip(task: str):
+    return await dataBase.Events.find({"description": {"$regex": task, "$options": "i"}}).to_list(length=None)
 
-async def dateFiltering(date: str) -> dict:
-    return await dataBase.Events.find({"date":{"$regex": date}})
+async def dateFiltering(date: str):
+    return await dataBase.Events.find({"date":{"$regex": date, "$options": "i"}}).to_list(length=None)
 
-async def userFiltering(role: str) -> dict:
-    return await dataBase.Events.find({"attendees.role":{"$regex": role}})
+async def userFiltering(role: str):
+    return await dataBase.Events.find({"attendees.role":role}).to_list(length=None)
 
-async def combination(title = None, description = None, date = None, role = None) -> dict:
+async def combination(title = None, description = None, date = None, role = None):
+
+    results = []
+
     if title:
-        searchByName(title)
+        results.append(await searchByName(title))
     if description:
-        searchByTaskDescrip(description)
+        results.append(await searchByTaskDescrip(description))
     if date:
-        dateFiltering(date)
+        results.append(await dateFiltering(date))
     if role:
-        userFiltering(role)
+        results.append(await userFiltering(role))
