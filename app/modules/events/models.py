5a229c82fb527d1267b8b57cde8c03c018802458
arwaa29 <140168581+arwaa29@users.noreply.github.com>
2025-11-28 04:56:18 +0200

    helper function

--- app/modules/events/models.py
+++ app/modules/events/models.py
@@ -1,7 +1,7 @@
 from bson import ObjectId
 
-def event_helper(event) -> dict:
-    return {
+def event_helper(event, attendee_id: str = None, status: str = None) -> dict:
+    event_data = {
         "id": str(event["_id"]),
         "title": event["title"],
         "date": (event["date"]),
@@ -10,3 +10,11 @@ def event_helper(event) -> dict:
         "description": event["description"],
         "organizer_id": event["organizer_id"]
     }
+
+    if attendee_id:
+        event_data["attendee_id"] = attendee_id
+    if status:
+        event_data["status"] = status
+    return event_data
+
+
