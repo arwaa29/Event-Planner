29a1ed23c528c704743c4a9d7e229a1adc628b31
arwaa29 <140168581+arwaa29@users.noreply.github.com>
2025-11-28 04:56:48 +0200

    logic

--- app/modules/events/schemas.py
+++ app/modules/events/schemas.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 from datetime import date, time
+from typing import Optional
 
 class CreateEvent(BaseModel):
         title: str
@@ -25,16 +26,28 @@ class OrganizedEventResponse(BaseModel):
         allow_population_by_field_name = True
 
 class InvitedEventResponse(BaseModel):
-    id: str = Field(..., alias="_id")
+    id: str
     title: str
     date: date
     time: time
     location: str
     description: str
     attendee_id: str
+    status: str
 
     class Config:
         orm_mode = True
         allow_population_by_field_name = True
+
+class InvitedUser(BaseModel):
+    event_id:str
+    email: Optional[EmailStr] = None
+    username: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+class DeleteEvent(BaseModel):
+    event_id:str
 
 
