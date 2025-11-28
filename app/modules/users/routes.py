4c431484d9d28a1b9a7474b38ff8d3fce560e3c8
arwaa29 <140168581+arwaa29@users.noreply.github.com>
2025-11-28 19:38:24 +0200

    APIs

--- app/modules/users/routes.py
+++ app/modules/users/routes.py
@@ -7,7 +7,7 @@ userRouter = APIRouter(prefix="/users", tags=["Users"])
 @userRouter.post("/register", response_model= RegisterResponse)
 async def register(request: Signup):
     result = await signUp(request.first_name, request.last_name, request.email,
-                            request.username, request.password, request.role)
+                            request.username, request.password)
     if "token" not in result:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=result["message"])
     return result
