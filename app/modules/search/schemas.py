146449dec8ba6c69616ad9a68d22a6f7128f8e55
arwaa29 <140168581+arwaa29@users.noreply.github.com>
2025-11-28 18:51:15 +0200

    schema

--- app/modules/search/schemas.py
+++ app/modules/search/schemas.py
@@ -1,12 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional
 
-class SearchFilters(BaseModel):
-    title: Optional[str]
-    date: Optional[str]
-    description: Optional[str]
-    role: Optional[str]
-
-    model_config = {
-    "from_attributes": True
-    }
+class EventSearch(BaseModel):
+    keyword: Optional[str] = None
+    date: Optional[str] = None
+    role: Optional[str] = None
